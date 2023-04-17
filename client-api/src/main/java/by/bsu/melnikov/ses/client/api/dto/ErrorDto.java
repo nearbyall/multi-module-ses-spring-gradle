5f004ded7f5db76f8bf159657585ba3b1194e976
nearbyall <notzloyhuman@gmail.com>
2023-04-17 15:01:34 +0300

    Initial commit

---
+++ client-api/src/main/java/by/bsu/melnikov/ses/client/api/dto/ErrorDto.java
@@ -0,0 +1,18 @@
+package by.bsu.melnikov.ses.client.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ErrorDto {
+
+    String error;
+
+    @JsonProperty("error_description")
+    String errorDescription;
+}
